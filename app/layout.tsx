f0106bdc37e6119b9b3af83e3a618b50fff6ff4d
antoniovalentini <antonio.valentini@live.it>
2025-03-28 17:50:21 +0100

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,20 @@
-import type { Metadata } from 'next'
+import { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Cinghialines',
   description: 'Tracking the shipping status for something very special',
   generator: 'v0.dev',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
